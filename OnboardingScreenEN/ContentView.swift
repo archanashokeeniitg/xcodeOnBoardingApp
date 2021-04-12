faa1a5310724437cdfc36afee77b0966b16d1b63
archanashokeeniitg <archanashokeeniitg@gmail.com>
2021-04-11 18:36:54 -0700

    Initial Commit

---
+++ OnboardingScreenEN/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnboardingScreenEN
+//
+//  Created by Archana Shokeen on 4/11/21.
+//  Copyright © 2021 Archana Shokeen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
